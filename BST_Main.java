15bf646226b3d208de83c07da304b4a8cb50c29d
Dheeraj Kriplani <163651371+Dheeraj26-02@users.noreply.github.com>
2024-10-04 18:31:38 +0530

    Create BST_Main.java

---
+++ BST_Main.java
@@ -0,0 +1,14 @@
+public class BSTMain {
+    public static void main(String[] args) {
+        int[] in = { 10, 20, 30, 40, 50, 60, 70, 80 };
+        BSTConstruct bst = new BSTConstruct();
+        bst.Binary_Search_Tree(in);
+        bst.pre();
+        System.out.println();
+        System.out.println(bst.max());
+        System.out.println();
+        System.out.println(bst.find(170));
+        bst.delete(40);
+        bst.pre();
+    }
+}
